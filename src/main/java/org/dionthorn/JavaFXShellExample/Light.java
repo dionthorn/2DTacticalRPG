490e8dd0a61d0030cd02e6635d8291888f0c3dab
dionthorn <dionthorn@gmail.com>
2020-08-26 21:12:00 -0600

    JavaFX Example - Interactive
    Added a simple system so you can generate up to 10 'Lights' which will randomly change position over time.
    
    When you click a location it will add a Light to the lights array with the (x, y) of the mouse.

---
+++ src/main/java/org/dionthorn/JavaFXShellExample/Light.java
@@ -0,0 +1,39 @@
+package org.dionthorn.JavaFXShellExample;
+
+public class Light {
+
+    public static int LIGHT_SIZE = 5;
+    private int x;
+    private int y;
+    private int lastMoved = 0;
+
+    public Light(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getLastMoved() {
+        return lastMoved;
+    }
+
+    public void setLastMoved(int lastMoved) {
+        this.lastMoved = lastMoved;
+    }
+    
+}
--- src/main/java/org/dionthorn/JavaFXShellExample/Run.java
+++ src/main/java/org/dionthorn/JavaFXShellExample/Run.java
@@ -11,7 +11,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Run extends Application {
@@ -24,17 +24,41 @@ public class Run extends Application {
     private long currentTime;
     private GraphicsContext gc;
 
-    public void render() {
+    // This isn't needed for minimal run environment
+    private Random rand = new Random();
+    private int maxLights = 20;
+    private Light[] lights = new Light[maxLights];
+    //
 
+    public void render() {
+        // This isn't needed for minimal run environment
+        gc.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        for(Light light: lights) {
+            if(light != null) {
+                gc.setFill(Color.WHITE);
+                gc.fillRect(light.getX(), light.getY(), 5, 5);
+            }
+        }
+        //
     }
 
     public void update() {
-
+        // This isn't needed for minimal run environment
+        int timeCap = 100;
+        for(Light light: lights) {
+            if(light != null && light.getLastMoved() <= timeCap) {
+                light.setLastMoved(light.getLastMoved() + 1);
+            } else if(light != null) {
+                light.setX(rand.nextInt(SCREEN_WIDTH - Light.LIGHT_SIZE));
+                light.setY(rand.nextInt(SCREEN_HEIGHT - Light.LIGHT_SIZE));
+                light.setLastMoved(0);
+            }
+        }
+        //
     }
 
     @Override
     public void start(Stage primaryStage) {
-
         // Provide basic contexts and setup the window
         primaryStage.setTitle("Example Shell " + PROGRAM_VERSION);
         primaryStage.setResizable(false);
@@ -58,6 +82,15 @@ public class Run extends Application {
             int mouseX = (int) mouseEvent.getX();
             int mouseY = (int) mouseEvent.getY();
             System.out.printf("Mouse Clicked At: (%d, %d)%n", mouseX, mouseY);
+
+            // This isn't needed for minimal run environment
+            for(int i=0; i<lights.length; i++) {
+                if(lights[i] == null) {
+                    lights[i] = new Light(mouseX, mouseY);
+                    break;
+                }
+            }
+            //
         });
 
         // Setup animator
